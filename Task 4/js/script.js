ca4112f517d48d3cf2946c0da0204f437f667e4a
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2019-04-18 17:15:48 +1000

    added possibility to show result square via js using mathml

--- Task 4/js/script.js
+++ Task 4/js/script.js
@@ -1,8 +1,10 @@
+isChecked = false;
+
 function square(a, b) {
     return Math.PI * a * b;
 }
 
 function insertHTML(a, b) {
-    let number = document.getElementById('insert-number');
-    number.innerHTML = square(a,b);
+    let number = document.getElementById('result');
+    number.innerHTML = '<span class="mn" id="MathJax-Span-5" style="font-family: MathJax_Main; padding-left: 0.272em;">' + square(a, b) + '</span>';
 }
