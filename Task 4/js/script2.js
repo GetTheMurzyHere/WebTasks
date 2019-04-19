47e5d0323fe0b9f93cba179fd51865601ff4a2b0
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2019-04-19 19:54:26 +1000

    created workspace for task 2

---
+++ Task 4/js/script2.js
@@ -0,0 +1,4 @@
+function result(x) {
+    let xArgument = document.getElementById('argumentX');
+    xArgument.innerHTML = '<span class="mn" id="MathJax-Span-5" style="font-family: MathJax_Main; padding-left: 0.272em; padding-right: 0.272em">' + x + '</span>';
+}
