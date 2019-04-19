7d0881cce6fa6e259f2afb0faa3ff17c35edadbc
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2019-04-19 13:20:39 +1000

    added lines to show ellipse semi-radius

--- Task 4/js/script.js
+++ Task 4/js/script.js
@@ -1,10 +1,33 @@
-isChecked = false;
+function drawEllipse(a, b) {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (a != 0 && b != 0) {
+        ctx.beginPath();
+        ctx.setLineDash([]);
+        ctx.ellipse(200, 200, a, b, 0, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.moveTo(200 - a, 200);
+        ctx.setLineDash([3, 2]);
+        ctx.lineTo(200, 200);
+        ctx.lineTo(200, 200 - b);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+}
 
 function square(a, b) {
     return Math.PI * a * b;
 }
 
-function insertHTML(a, b) {
+function result(a, b) {
     let number = document.getElementById('result');
     number.innerHTML = '<span class="mn" id="MathJax-Span-5" style="font-family: MathJax_Main; padding-left: 0.272em;">' + square(a, b) + '</span>';
+    drawEllipse(a, b);
 }
