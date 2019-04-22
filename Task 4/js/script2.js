2ff31f0d091629631b26bb73a8df7c45b7039911
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2019-04-22 16:26:31 +1000

    fixed wrong sinus calculation and changed accuracy

--- Task 4/js/script2.js
+++ Task 4/js/script2.js
@@ -4,15 +4,8 @@ function factorial(n) {
 
 function result(x, count) {
     let sum = 0;
-    let exp = 1;
-    for (let i = 1; i <= count; i++) {
-        if (i % 2 == 1) {
-            sum += Math.pow(x, exp) / factorial(i);
-        }
-        else {
-            sum -= Math.pow(x, exp) / factorial(i);
-        }
-        exp += 2;
+    for (let i = 0; i < count; i++) {
+        sum += Math.pow(-1, i) * ((Math.pow(x, 2 * i + 1))/(factorial(2 * i + 1)));
     }
     let finalSum = document.getElementById('result-sum');
     finalSum.innerHTML = '<span class="mn" id="MathJax-Span-5" style="font-family: MathJax_Main; padding-left: 0.272em; padding-right: 0.272em">' + sum + '</span>';
