21d85769faaf6c6363617d35b105a13dcb372821
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2019-04-22 11:55:47 +1000

    updated code

--- Task 4/js/script2.js
+++ Task 4/js/script2.js
@@ -3,6 +3,7 @@ function factorial(n) {
 }
 
 function result(x, count) {
+    x = x * 180 / Math.PI;
     let sum = 0;
     let exp = 1;
     for (let i = 1; i <= count; i++) {
@@ -16,8 +17,5 @@ function result(x, count) {
     }
     let finalSum = document.getElementById('result-sum');
     finalSum.innerHTML = '<span class="mn" id="MathJax-Span-5" style="font-family: MathJax_Main; padding-left: 0.272em; padding-right: 0.272em">' + sum + '</span>';
-    
-    let xArgument = document.getElementById('argumentX');
-    xArgument.innerHTML = '<span class="mn" id="MathJax-Span-5" style="font-family: MathJax_Main; padding-left: 0.272em; padding-right: 0.272em">' + x + '</span>';
 
 }
