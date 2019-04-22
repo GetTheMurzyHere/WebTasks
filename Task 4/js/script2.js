5dfc1d786629de66a13b0662e1ee192fcd38611e
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2019-04-22 10:10:51 +1000

    added sinus formula as general

--- Task 4/js/script2.js
+++ Task 4/js/script2.js
@@ -1,4 +1,21 @@
-function result(x) {
+function factorial(n) {
+    return (n == 1) ? 1 : n * factorial(n - 1);
+}
+
+function result(x, count) {
     let xArgument = document.getElementById('argumentX');
     xArgument.innerHTML = '<span class="mn" id="MathJax-Span-5" style="font-family: MathJax_Main; padding-left: 0.272em; padding-right: 0.272em">' + x + '</span>';
+    let sum = 0;
+    let exp = 1;
+    for (let i = 1; i <= count; i++) {
+        if (i % 2 == 1) {
+            sum += Math.pow(x, exp) / factorial(i);
+        }
+        else {
+            sum -= Math.pow(x, exp) / factorial(i);
+        }
+        exp += 2;
+    }
+    let finalSum = document.getElementById('result-sum');
+    finalSum.innerHTML += '<span class="mn" id="MathJax-Span-5" style="font-family: MathJax_Main; padding-left: 0.272em; padding-right: 0.272em">' + sum + '</span>';
 }
