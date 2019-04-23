d75724502032b9eb0df984ac3e70bde9ae237efb
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2019-04-24 03:12:04 +1000

    added mathml matrix and posibility to push its values

--- Task 4/js/script3.js
+++ Task 4/js/script3.js
@@ -8,6 +8,10 @@ enterButton.addEventListener('click', result);
 var inputsArray = document.getElementsByClassName('form-control');
 var matrix = []; negativeMatrix = [];
 
+function div(val, by){
+    return (val - val % by) / by;
+}
+
 function result() {
     for (let i = 0; i < 5; i++) {
         matrix[i] = inputsArray[i].value.split(' ').map(Number);
@@ -26,4 +30,17 @@ function result() {
             }
         }
     }
+
+    document.getElementById('input-block').style.display = "none";
+    enterButton.style.display = "none";
+
+    document.getElementById('matrix-result-block').className = "d-flex justify-content-around";
+    document.getElementById('workspace').className = "container";
+
+    let numbersArray = document.getElementsByClassName('result-number');
+
+    for (let i = 0; i < 40; i++) {
+        numbersArray[i].innerHTML = '<span class="mn" id="MathJax-Span-5" style="font-family: MathJax_Main;">' + matrix[i % 5][div(i, 5)] + '</span>'
+    }
+
 }
