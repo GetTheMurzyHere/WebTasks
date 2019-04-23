70969b7bd8fe2d3172b54dac48d07414e0da68aa
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2019-04-24 00:47:50 +1000

    added matrix fill

---
+++ Task 4/js/script3.js
@@ -0,0 +1,24 @@
+var enterButton = document.getElementById('enter-btn');
+enterButton.addEventListener('click', result);
+
+var inputsArray = document.getElementsByClassName('form-control');
+var matrix = []; negativeMatrix = [];
+
+function result() {
+    for (let i = 0; i < 5; i++) {
+        matrix[i] = inputsArray[i].value.split(' ').map(Number);
+        if (matrix[i].length != 8) {
+            alert("Некорректно введены значения матрицы!");
+            return;
+        }
+        negativeMatrix[i] = [];
+        for (let j = 0; j < 8; j++) {
+            if (matrix[i][j] != 0) {
+                negativeMatrix[i][j] = -matrix[i][j];
+            }
+            else {
+                negativeMatrix[i][j] = 0;
+            }
+        }
+    }
+}
