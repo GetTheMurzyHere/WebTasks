8b451fbcc133c51f1cdcac4f3ba7219b9e0bbcbc
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2019-04-25 23:30:33 +1000

    changed input form and checked input validity

--- Task 4/js/script4.js
+++ Task 4/js/script4.js
@@ -2,16 +2,49 @@ $(function () {
     $('[data-toggle="popover"]').popover()
 })
 
-var enterSizeButton = document.getElementById('enter-size-btn');
-enterSizeButton.addEventListener('click', getMatrixSize);
-enterSizeButton.addEventListener('click', showMatrix);
+var enterSizeButton = document.getElementById('generate-btn');
+enterSizeButton.addEventListener('click', getMinAndMax);
+enterSizeButton.addEventListener('click', getArray);
 
-function getMatrixSize() {
-    return Number(document.getElementById('size').value);
+var arraySize, matrixSize;
+
+function getMinAndMax() {
+    let minMaxArrayValues = document.getElementById('min-max-array').value.split(' ').map(Number);
+    let minMaxMatrixValues = document.getElementById('min-max-matrix').value.split(' ').map(Number);
+
+    if (minMaxArrayValues[0] == 0 || minMaxArrayValues[1] == 0 || minMaxMatrixValues[0] == 0 || minMaxMatrixValues[1] == 0) {
+        alert('Границы диапазона не могут быть равны нулю!');
+        return;
+    }
+    else {
+        if (minMaxArrayValues[0] > minMaxArrayValues[1] || minMaxMatrixValues[0] > minMaxMatrixValues[1]) {
+            alert('Верхняя граница диапазона не может быть меньше нижней!');
+            return;
+        }
+        else {
+            if (minMaxArrayValues[0] < 0 || minMaxMatrixValues[0] < 0) {
+                alert('Размерность массива или матрицы не может быть отрицательной!');
+                return;
+            }
+        }
+    }
+
+    arraySize = getRandomInt(minMaxArrayValues[0], minMaxArrayValues[1]);
+    matrixSize = getRandomInt(minMaxMatrixValues[0], minMaxMatrixValues[1]);
+
+    showMatrix();
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getArray(size) {
+
 }
 
 function showMatrix() {
-    document.getElementById('input-size').style.display = "none";
+    document.getElementById('input-generate').style.display = "none";
 }
 
 
