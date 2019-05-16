b0f547a465e27f1baa14075aceaa2f6e1398d06f
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2019-05-17 09:47:28 +1000

    changed first task to recursion

--- Task 5/script/script1.js
+++ Task 5/script/script1.js
@@ -4,49 +4,55 @@ var animals = [
         children: [
             {
                 name: 'Млекопитающие',
-                children: ['Коровы', 'Ослы', 'Собаки', 'Тигры']
+                children: [
+                    { name: 'Коровы' },
+                    { name: 'Ослы' },
+                    { name: 'Собаки' },
+                    { name: 'Тигры' },
+                ]
             },
-
             {
                 name: 'Другие',
-                children: ['Змеи', 'Птицы', 'Ящерицы']
-            }
+                children: [
+                    { name: 'Змеи' },
+                    { name: 'Птицы' },
+                    { name: 'Ящерицы' },
+                ]
+            },
         ]
     },
-
     {
         name: 'Рыбы',
         children: [
             {
                 name: 'Аквариумные',
-                children: ['Гуппи', 'Скалярии']
+                children: [
+                    { name: 'Гуппи' },
+                    { name: 'Скалярии' },
+                ]
             },
-
             {
                 name: 'Морские',
-                children: ['Морская форель']
-            }
+                children: [
+                    { name: 'Морская форель' },
+                ]
+            },
         ]
-    }
-]
+    },
+];
 
-var kingdoms = document.createElement('ul');
-document.getElementsByTagName('section')[0].appendChild(kingdoms);
-
-animals.forEach(kingdom => {
-    kingdoms.innerHTML += '<li>' + kingdom.name + '</li>';
-    var types = document.createElement('ul');
-    // Добавление в список царства класса
-    kingdom.children.forEach(type => {
-        types.innerHTML += '<li>' + type.name + '</li>';
-        var _animals = document.createElement('ul');
-        // Добавление в список класса название животных
-        type.children.forEach(animal => {
-            _animals.innerHTML += '<li>' + animal + '</li>';
-        });
-
-        types.appendChild(_animals);
+function generate(array) {
+    var html = '<ul>';
+    array.forEach(item => {
+        var li = '<li>';
+        html += li + item.name;
+        if (item.children) {
+            html += generate(item.children);
+        }
+        html += '</li>';
     });
+    html += '</ul>';
+    return html;
+}
 
-    kingdoms.appendChild(types);
-});
+document.body.innerHTML = generate(animals);
