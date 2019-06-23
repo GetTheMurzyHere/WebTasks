20c91bdbf73d28b5efa0bb775711c815a4ec624b
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2019-06-23 15:05:02 +1000

    added life bar and losing lifes possibility

--- Task 8/js/script.js
+++ Task 8/js/script.js
@@ -1,5 +1,8 @@
+let handyFont;
+
 let cannonImg;
 let klevchImg;
+let heartImg;
 
 const WIDTH = 850;
 const HEIGHT = 500;
@@ -7,25 +10,22 @@ const HEIGHT = 500;
 let cannon;
 let shots = [];
 let enemies = [];
-
-function spawnEnemies(count) {
-  let delta = 0;
-  for (let i = 0; i < count; i++) {
-    let enemy = new AirEnemy(klevchImg, klevchImg.width / 10);
-    enemy.posX += delta;
-    enemies.push(enemy);
-    delta += 175;
-  }
-}
+let lifes = [];
 
 function preload() {
   cannonImg = loadImage('assets/img/cannon.png');
   klevchImg = loadImage('assets/img/klevch.png');
+  heartImg = loadImage('assets/img/heart.png');
+  
+  handyFont = loadFont('assets/fonts/hAndy.ttf');
 }
+
 
 function setup() {
   cnv = createCanvas(WIDTH, HEIGHT);
   background(164, 217, 224);
+
+  lifes = [heartImg, heartImg, heartImg];
 
   cannon = new Cannon(cannonImg, cannonImg.width / 2, cannonImg.height / 2)
   cnv.mouseClicked(cannon.shoot);
@@ -40,11 +40,15 @@ function setup() {
   spawnEnemies(10);
 
   frameRate(60);
-
 }
 
 function draw() {
   background(164, 217, 224);
+  textFont(handyFont);
+  fill('#000000');
+  textSize(32);
+  let score = "Очки: "
+  text(score, 130, 25)
   if (shots.length != 0) {
     shots.forEach((shot) => {
       if ((shot.posX >= WIDTH || shot.posY >= HEIGHT)) {
@@ -70,12 +74,38 @@ function draw() {
     })
   }
   enemies.forEach((enemy) => {
+    if(enemy.posX <= 35) {
+      lifes.pop();
+      enemies.shift();
+    }
     enemy.render();
   })
+  drawLifes();
+
+  fill("#ffffff");
+  line(35, 500, 35, 0)
   push();
   translate(30, 485);
   cannon.move();
   pop();
   fill("rgb(18, 187, 74)");
   circle(13, HEIGHT + 25, cannon.width);
+}
+
+function spawnEnemies(count) {
+  let delta = 0;
+  for (let i = 0; i < count; i++) {
+    let enemy = new AirEnemy(klevchImg, klevchImg.width / 10);
+    enemy.posX += delta;
+    enemies.push(enemy);
+    delta += 175;
+  }
+}
+
+function drawLifes() {
+  let x = 50;
+  lifes.forEach((life) => {
+    image(life, x, 16, life.width / 10, life.height / 10);
+    x += 30
+  })
 }
